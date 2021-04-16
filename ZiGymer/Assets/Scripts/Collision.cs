8ed88f006477e13b7c1236b9c194d8077346e405
ThanhQuang <thanhhung180498@gmail.com>
2021-04-16 21:40:41 +0700

    admin commit end.

--- ZiGymer/Assets/Scripts/Collision.cs
+++ ZiGymer/Assets/Scripts/Collision.cs
@@ -14,6 +14,22 @@ public class Collision : MonoBehaviour
     public Collider2D leftThighsCollider;
     public Collider2D rightThighsCollider;
     public Sprite thighsSource;
+    public Collider2D backCollider;
+    public Sprite backSource;
+    public Collider2D waistCollider;
+    public Sprite waistSource;
+    public Collider2D leftHindThighsCollider;
+    public Collider2D rightHindThighsCollider;
+    public Sprite hindthighsSource;
+    public Collider2D leftCalfCollider;
+    public Collider2D rightCalfCollider;
+    public Sprite calfSource;
+    public Collider2D leftBicepsCollider;
+    public Collider2D rightBicepsCollider;
+    public Sprite BicepsSource;
+    public Collider2D leftShoulderCollider;
+    public Collider2D rightShoulderCollider;
+    public Sprite ShoulderSource;
 
     // Start is called before the first frame update
     void Start()
@@ -52,6 +68,54 @@ public class Collision : MonoBehaviour
             string title = "CƠ ĐÙI";
             string content = System.IO.File.ReadAllText(@"Assets/Content/thighs.txt");
             gameController.GetComponent<Controller>().ShowContent(title, content, Input.mousePosition, thighsSource);
+        }
+        if (collision == backCollider)
+        {
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            worldPosition.z = 0;
+            string title = "CƠ LƯNG";
+            string content = System.IO.File.ReadAllText(@"Assets/Content/back.txt");
+            gameController.GetComponent<Controller>().ShowContent(title, content, Input.mousePosition, backSource);
+        }
+        if (collision == waistCollider)
+        {
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            worldPosition.z = 0;
+            string title = "CƠ EO";
+            string content = System.IO.File.ReadAllText(@"Assets/Content/waist.txt");
+            gameController.GetComponent<Controller>().ShowContent(title, content, Input.mousePosition, waistSource);
+        }
+        if (collision == leftHindThighsCollider || collision == rightHindThighsCollider)
+        {
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            worldPosition.z = 0;
+            string title = "CƠ ĐÙI SAU";
+            string content = System.IO.File.ReadAllText(@"Assets/Content/hindthighs.txt");
+            gameController.GetComponent<Controller>().ShowContent(title, content, Input.mousePosition, hindthighsSource);
+        }
+        if (collision == leftCalfCollider || collision == rightCalfCollider)
+        {
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            worldPosition.z = 0;
+            string title = "CƠ BẮP CHÂN";
+            string content = System.IO.File.ReadAllText(@"Assets/Content/calf.txt");
+            gameController.GetComponent<Controller>().ShowContent(title, content, Input.mousePosition, calfSource);
+        }
+        if (collision == leftBicepsCollider || collision == rightBicepsCollider)
+        {
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            worldPosition.z = 0;
+            string title = "CƠ BẮP TAY";
+            string content = System.IO.File.ReadAllText(@"Assets/Content/calf.txt");
+            gameController.GetComponent<Controller>().ShowContent(title, content, Input.mousePosition, BicepsSource);
+        }
+        if (collision == leftShoulderCollider || collision == rightShoulderCollider)
+        {
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            worldPosition.z = 0;
+            string title = "CƠ BẮP TAY";
+            string content = System.IO.File.ReadAllText(@"Assets/Content/calf.txt");
+            gameController.GetComponent<Controller>().ShowContent(title, content, Input.mousePosition, ShoulderSource);
         }
     }
 
